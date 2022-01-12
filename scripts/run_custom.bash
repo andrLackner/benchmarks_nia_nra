1760944db26f7fc85c7e084dd5f0ffffce627959
Lackner Andreas <lackner.andr@gmail.com>
2022-01-12 21:52:56 +0100

    Additional results

--- scripts/run_custom.bash
+++ scripts/run_custom.bash
@@ -2,8 +2,8 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$SCRIPT_DIR/run.bash z3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/norm
-$SCRIPT_DIR/run.bash z3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/denorm
+#$SCRIPT_DIR/run.bash z3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/norm
+#$SCRIPT_DIR/run.bash z3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/denorm
 
-$SCRIPT_DIR/run.bash vampireZ3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/denorm
-$SCRIPT_DIR/run.bash vampireZ3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/denorm
+#$SCRIPT_DIR/run.bash vampireZ3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/denorm
+$SCRIPT_DIR/run.bash vampireZ3 $SCRIPT_DIR/../out $SCRIPT_DIR/../benchmarks/norm

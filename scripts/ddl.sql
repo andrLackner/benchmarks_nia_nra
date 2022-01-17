50c66412a7ada98dc8f41a361746a94772fbafda
Lackner Andreas <lackner.andr@gmail.com>
2022-01-17 12:54:56 +0100

    Add further results and changes in scripts

--- scripts/ddl.sql
+++ scripts/ddl.sql
@@ -19,6 +19,64 @@ CREATE TABLE "result" (
     "len_proof" NUMBER NOT NULL,
     "time" NUMBER NOT NULL,
     "benchmark_id" TEXT NOT NULL,
+    "polymul" NUMBER DEFAULT 0,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("benchmark_id") REFERENCES "benchmark" ("id")
-)
+);
+
+
+CREATE VIEW result_check_vampire ("type", "cnt") AS
+    SELECT "type", COUNT(*) FROM result
+        JOIN benchmark on benchmark.id = result.benchmark_id
+        WHERE result.solver = 'vampireZ3'
+        GROUP BY "type";
+
+CREATE VIEW result_check_z3 ("type", "cnt") AS
+    SELECT "type", COUNT(*) FROM result
+        JOIN benchmark on benchmark.id = result.benchmark_id
+        WHERE result.solver = 'vampireZ3'
+        GROUP BY "type";
+
+CREATE VIEW result_check ("type", "vampire", "z3") AS
+    SELECT result_check_vampire."type", result_check_vampire.cnt AS vampire, result_check_z3.cnt AS z3
+        FROM result_check_vampire
+        JOIN result_check_z3 ON result_check_vampire.type = result_check_z3.type;
+
+CREATE VIEW baseline (benchmark_id, vampire, z3) AS
+    SELECT benchmark.id, 
+        CASE vamp.result 
+            WHEN "solved" THEN vamp.time
+            ELSE -1
+        END,
+        CASE z3.result 
+            when "solved" THEN z3.time
+            ELSE -1
+        END FROM benchmark
+        JOIN result vamp on benchmark.id = vamp.benchmark_id AND vamp.solver = 'vampireZ3'
+        JOIN result z3 on benchmark.id = z3.benchmark_id AND z3.solver = 'z3'
+        WHERE type = 'baseline';
+
+CREATE VIEW denorm_std (benchmark_id, len, result) AS 
+    SELECT benchmark_id, len_proof, result FROM result 
+        WHERE benchmark_id LIKE "%_denorm_%" 
+        AND solver="vampireZ3";
+
+CREATE VIEW denorm_mul_opt (benchmark_id, len, result, polymul) AS 
+    SELECT benchmark_id, len_proof, result, polymul FROM result 
+        WHERE benchmark_id LIKE "%_denorm_%" 
+        AND solver="vampireZ3polymulOpt";
+
+CREATE VIEW denorm_mul (benchmark_id, len, result, polymul) AS 
+    SELECT benchmark_id, len_proof, result, polymul FROM result 
+        WHERE benchmark_id LIKE "%_denorm_%" 
+        AND solver="vampireZ3polymul";
+
+CREATE VIEW overview_denorm_std_vs_mul (result, cnt) AS 
+    SELECT D1.result || "/" || D2.result AS res, count(*) FROM denorm_std D1 
+        JOIN denorm_mul D2 ON D1.benchmark_id = D2.benchmark_id 
+        GROUP BY res;
+
+CREATE VIEW overview_denorm_std_vs_opt (result, cnt) AS 
+    SELECT D1.result || "/" || D2.result AS res, count(*) FROM denorm_std D1 
+        JOIN denorm_mul_opt D2 ON D1.benchmark_id = D2.benchmark_id 
+        GROUP BY res;
--- scripts/evaluate.bash
+++ scripts/evaluate.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 function get_query() {
-    COLS="\"id\", \"solver\", \"result\", \"len_proof\", \"time\", \"benchmark_id\""
-    QUERY="INSERT INTO \"result\" ($COLS) VALUES (\"$1\", \"$2\", \"$3\", $4, $5, \"$6\");"
+    COLS="\"id\", \"solver\", \"result\", \"len_proof\", \"time\", \"benchmark_id\", \"polymul\""
+    QUERY="INSERT INTO \"result\" ($COLS) VALUES (\"$1\", \"$2\", \"$3\", $4, $5, \"$6\", $7);"
     echo $QUERY
 }
 
 function extract_z3_results() {
-    local file=$1
-    local benchmark_id=$2
-    local temp=$3
+    local solver=$1
+    local file=$2
+    local benchmark_id=$3
+    local temp=$4
 
 
     local result=$(head -n 1 $file)
@@ -31,32 +32,38 @@ function extract_z3_results() {
     fi
 
     local id=$(uuidgen)
-    get_query $id "z3" $result $len $time $benchmark_id >> $temp
+    get_query $id $solver $result $len $time $benchmark_id -1 >> $temp
 }
 
 function extract_vampire_results() {
-    local file=$1
-    local benchmark_id=$2
-    local temp=$3
-    local end=$(tail -n 10 $file)
+    local solver=$1
+    local file=$2
+    local benchmark_id=$3
+    local temp=$4
+    local end=$(tail -n 15 $file)
     local termination_reason=$(echo "$end" | grep "% Termination reason: ")
 
     local result="unsolved"
     local time=300
     local id=$(uuidgen)
+    local polymul=0
     if [[ "$termination_reason" == "% Termination reason: Refutation" ]]; then
         result="solved"
         time_elapsed=$(echo "$end" | grep "% Time elapsed: ")
+
+        if grep -q "\\[polynomial multiplication" "$file"; then
+            polymul=1
+        fi
         
         if [[ $time_elapsed =~ ^%[[:space:]]Time[[:space:]]elapsed:[[:space:]]+([0-9]+(\.[0-9]+))[[:space:]]s.*$ ]]; then
             local time=${BASH_REMATCH[1]}
-            local len=$(sed  -nE "s/^([0-9]+)\.[[:space:]].*$/\1/p" $file | tail -n 1)
-            get_query $id "vampireZ3" $result $len $time $benchmark_id >> $temp
+            local len=$(sed  -nE "s/^([0-9]+)\.[[:space:]].*$/\1/p" $file | wc -l )
+            get_query $id $solver $result $len $time $benchmark_id $polymul >> $temp
         else
-            echo "Cannot process $1. Unexpected format"
+            echo "Cannot process $2. Unexpected format"
         fi
     else
-        get_query $id "vampireZ3" $result -1 $time $benchmark_id >> $temp
+        get_query $id $solver $result -1 $time $benchmark_id -1 >> $temp
     fi
 }
 
@@ -76,17 +83,21 @@ for FILE in $FILES; do
     ID="$(basename -s ".out" $FILE).smt2"
     SOLVER=$(basename $(dirname $FILE))
     
-    if [[ "$SOLVER" = "z3" ]] || [[ "$SOLVER" = "z3_test" ]]; then
-        SOVLER="z3"
-        extract_z3_results $FILE $ID $TEMP_FILE
-    elif [[ "$SOLVER" = "vampireZ3" ]]; then
-        extract_vampire_results $FILE $ID $TEMP_FILE
+    if [[ "$SOLVER" == "z3_test" ]]; then
+        extract_z3_results "z3" $FILE $ID $TEMP_FILE
+    elif [[ "$SOLVER" == "vampireZ3" ]]; then
+        extract_vampire_results "vampireZ3" $FILE $ID $TEMP_FILE
+    elif [[ "$SOLVER" == "vampireZ3polymul" ]]; then
+        extract_vampire_results "vampireZ3polymul" $FILE $ID $TEMP_FILE
+    elif [[ "$SOLVER" == "vampireDebug" ]]; then
+        extract_vampire_results "vampireZ3polymulOpt" $FILE $ID $TEMP_FILE
+    elif [[ "$SOLVER" == "z3" ]]; then
         continue
     else
         echo "Skipping $FILE. Unknown solver $SOLVER"
     fi
 done;
 
-
+echo "Executing query"
 sqlite3 $SCRIPT_DIR/../results.db < $TEMP_FILE
 rm $TEMP_FILE
